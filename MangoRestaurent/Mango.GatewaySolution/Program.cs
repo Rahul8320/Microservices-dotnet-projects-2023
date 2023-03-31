1abbf15659bab1f587a291b391706f555cf727e3
RAHUL DEY <rahuldey832000@gmail.com>
2023-03-31 17:48:00 +0530

    Ocelot done

--- MangoRestaurent/Mango.GatewaySolution/Program.cs
+++ MangoRestaurent/Mango.GatewaySolution/Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// -- add authentication token
+builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
+{
+    options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false
+    };
+});
+
+// -- add ocelot
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Welcome to Ocelot gateway --- From Rahul Dey!");
+app.UseOcelot();
 
 app.Run();
